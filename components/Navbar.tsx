2a0e0f1825f097bcc9bfbeb7fc9421c3bb2a6b4b
Naren <nroh555@aucklanduni.ac.nz>
2023-06-21 23:54:16 +1200

    Implemented a responsive navigation bar

---
+++ components/Navbar.tsx
@@ -0,0 +1,106 @@
+"use client"; // this is a client component
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-scroll/modules";
+import { usePathname } from "next/navigation";
+import { useTheme } from "next-themes";
+import { RiMoonFill, RiSunLine } from "react-icons/ri";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
+
+interface NavItem {
+  title: string;
+  page: string;
+}
+
+const NAV_TITLES: Array<NavItem> = [
+  {
+    title: "About",
+    page: "about",
+  },
+  {
+    title: "Projects",
+    page: "projects",
+  },
+  {
+    title: "Experience",
+    page: "experience",
+  },
+  {
+    title: "Contact",
+    page: "contact",
+  },
+];
+
+const Navbar = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  const [navBar, setNavBar] = useState(false); //State responsible for mobile or desktop view
+
+  return (
+    <header className="w-full mx-auto sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
+      <div className="justify-between lg:items-center lg:flex">
+        <div>
+          <div className="flex items-center justify-between">
+            <div className="sm:py-6 sm:block">
+              <img
+                src={"/logo.png"}
+                alt="logo"
+                className="w-20 h-20 mb-5 inline-block"
+              />
+              <h1 className="inline-block ps-5 text-3xl text-[#006AFA] font-bold">
+                Naren Rohan
+              </h1>
+            </div>
+            <div className="lg:hidden">
+              <button onClick = {() => setNavBar(!navBar)}>
+                {navBar ? <IoMdClose size={40} /> : <IoMdMenu size={40} />}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 lg:block lg:pb-0 lg:mt-0 ${
+              navBar ? "block" : "hidden"
+            }`}
+          >
+            <div className="lg:flex lg:space-x-10 space-y-8 lg:space-y-0">
+              {NAV_TITLES.map((label, index) => {
+                return (
+                    <Link
+                    key={index}
+                    to={label.page}
+                    className={
+                      "block lg:inline-block text-xl text-neutral-900 hover:text-[#006AFA] dark:text-neutral-100 dark:hover:text-[#4B91F1]"
+                    }
+                    activeClass="active"
+                    onClick={() => setNavBar(!navBar)}
+                  >
+                    {label.title}
+                  </Link>
+                )
+              })}
+              {currentTheme === "dark" ? (
+                <button
+                  onClick={() => setTheme("light")}
+                  className="border-2 border-solid border-white rounded-lg"
+                >
+                  <RiSunLine size={30} color="white" />
+                </button>
+              ) : (
+                <button
+                  onClick={() => setTheme("dark")}
+                  className= "border-2 border-solid border-black rounded-lg"
+                >
+                  <RiMoonFill size={30} />
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
