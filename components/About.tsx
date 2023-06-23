7b87a1e78a4ec21e3c1d263a85288e9dedc61296
Naren <nroh555@aucklanduni.ac.nz>
2023-06-23 14:04:15 +1200

    Implemented the projects section

--- components/About.tsx
+++ components/About.tsx
@@ -1,107 +1,133 @@
 "use client"; // this is a client component
-import React from 'react'
+import React from "react";
 import Image from "next/image";
+import { Link } from "react-scroll";
+import { TfiHandPointDown } from "react-icons/tfi";
+import { BsGithub } from "react-icons/bs";
 
 const skills = [
-    {
-        image: "/tech/html.png",
-        skill: "HTML"
-    },
-    {
-        image: "/tech/css.png",
-        skill: "CSS"
-    },
-    {
-        image: "/tech/javascript.png",
-        skill: "Javascript"
-    },
-    {
-        image: "/tech/git.png",
-        skill: "Git"
-    },
-    {
-        image: "/tech/figma.png",
-        skill: "Figma"
-    },
-    {
-        image: "/tech/reactjs.png",
-        skill: "ReactJS"
-    },
-    {
-        image: "/tech/tailwind.png",
-        skill: "TailwindCSS"
-    },
-    {
-        image: "/tech/typescript.png",
-        skill: "Typescript"
-    },
-    {
-        image: "/tech/java.png",
-        skill: "Java"
-    },
-    {
-        image: "/tech/python.png",
-        skill: "Python"
-    },
-    {
-        image: "/tech/firebase.png",
-        skill: "Firebase"
-    },
-    {
-        image: "/tech/aws.png",
-        skill: "AWS"
-    },
-    {
-        image: "/tech/gcp.png",
-        skill: "GCP"
-    },
-    {
-        image: "/tech/github.png",
-        skill: "Github"
-    }
-]
+  {
+    image: "/tech/html.png",
+    skill: "HTML",
+  },
+  {
+    image: "/tech/css.png",
+    skill: "CSS",
+  },
+  {
+    image: "/tech/javascript.png",
+    skill: "Javascript",
+  },
+  {
+    image: "/tech/git.png",
+    skill: "Git",
+  },
+  {
+    image: "/tech/figma.png",
+    skill: "Figma",
+  },
+  {
+    image: "/tech/reactjs.png",
+    skill: "ReactJS",
+  },
+  {
+    image: "/tech/tailwind.png",
+    skill: "TailwindCSS",
+  },
+  {
+    image: "/tech/typescript.png",
+    skill: "Typescript",
+  },
+  {
+    image: "/tech/java.png",
+    skill: "Java",
+  },
+  {
+    image: "/tech/python.png",
+    skill: "Python",
+  },
+  {
+    image: "/tech/firebase.png",
+    skill: "Firebase",
+  },
+  {
+    image: "/tech/aws.png",
+    skill: "AWS",
+  },
+  {
+    image: "/tech/gcp.png",
+    skill: "GCP",
+  },
+];
 
 const About = () => {
   return (
-    <section className = "sm:pt-20" id = "about">
-        <div className= "my-10 pb-10 lg:pt-10 lg:pb-10">
-            <h1 className="text-center font-bold text-4xl">
-                About Me!
-                <hr className="w-10 h-1 mx-auto my-4 bg-blue-700 border-0 rounded"></hr>
+    <section className="sm:pt-20" id="about">
+      <div className="my-10 pb-10 lg:pt-10 lg:pb-10">
+        <h1 className="text-center font-bold text-4xl">
+          About Me!
+          <hr className="w-10 h-1 mx-auto my-4 bg-blue-700 border-0 rounded"></hr>
+        </h1>
+        <div className="flex flex-col space-y-5 items-stretch jusitfy-center align-top md:flex-row md:text-left md:p-4 md:space-y-0 md:space-x-10">
+          <div>
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
+              Summary
             </h1>
-            <div className= "flex flex-col space-y-5 items-stretch jusitfy-center align-top md:flex-row md:text-left md:p-4 md:space-y-0 md:space-x-10">
-                <div>
-                    <h1 className= "text-center text-2xl font-bold mb-6 md:text-left">
-                        Summary
-                    </h1>
-                    <p>
-                    Hi, my name is Naren and I am an aspiring software engineer based in Auckland, New Zealand. Currently, I am pursuing a 
-                    Bachelor of Engineering (Honours) in Software Engineering at University of Auckland.
-                    </p>
-                    <br />
-                    <p>
-                    I enjoy working in within the realm of web development and machine learning and am really looking for opportunities 
-                    to gain practical experience in full-stack development and learn the entire software development life cycle.
-                    </p>
-                    <br />
-                    <p>
-                    I have a wide range of hobbies and passions that often times keep me busy apart from engineering. From playing sports,
-                    working out to hiking while relishing the beautiful New Zealand nature. 
-                    </p>
-                    <br />
-                </div>
-                <div>
-                    <h1 className= "text-center text-2xl font-bold mb-6 ml-4 md:text-left">My Skills</h1>
-                    <div className= "flex flex-wrap flex-row justify-center md:justify-start">
-                        {skills.map((item,index) => {
-                            return <Image className = "px-2 py-1 mr-1 mt-1" src = {item.image} alt = {item.skill} width = {85} height = {85} key = {index} />
-                        })}
-                    </div>
-                </div>
+            <p>
+              Hi, my name is Naren and I am an aspiring software engineer based
+              in Auckland, New Zealand. Currently, I am pursuing a Bachelor of
+              Engineering (Honours) in Software Engineering at University of
+              Auckland.
+            </p>
+            <br />
+            <p>
+              I enjoy working in within the realm of web development and machine
+              learning and am really looking for opportunities to gain practical
+              experience in full-stack development and learn the entire software
+              development life cycle.
+            </p>
+            <br />
+            <p>
+              I have a wide range of hobbies and passions that often times keep
+              me busy apart from engineering. From playing sports, working out
+              to hiking while relishing the beautiful New Zealand nature.
+            </p>
+            <br />
+          </div>
+          <div>
+            <h1 className="text-center text-2xl font-bold mb-6 ml-4 md:text-left">
+              My Skills
+            </h1>
+            <div className="flex flex-wrap flex-row justify-center md:justify-start">
+              {skills.map((item, index) => {
+                return (
+                  <Image
+                    className="px-2 py-1 mr-1 mt-1"
+                    src={item.image}
+                    alt={item.skill}
+                    width={85}
+                    height={85}
+                    key={index}
+                  />
+                );
+              })}
+               <BsGithub size={75} className="ml-2 px-2 py-1 mr-1 mt-1"/>
             </div>
+          </div>
         </div>
+        <div className="flex flex-row justify-center mt-1">
+            <Link
+               to = "projects"
+               activeClass="active"
+               smooth = {true}
+               duration={400}
+            >
+                <TfiHandPointDown className = "animate-bounce" size = {50}/>
+            </Link>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
---
+++ components/Projects.tsx
@@ -0,0 +1,84 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { BsGithub } from "react-icons/bs";
+
+const projects = [
+  {
+    name: "Taitamariki Potentia",
+    description:
+      "Project manager and developer of the web application with the aim of easing students wanting to step foot into the tech industry. This project was created using NextJS, Typescript and Firebase.",
+    image: "/projects/taitamariki.png",
+    github: "https://github.com/nroh555/taitam-portal",
+  },
+  {
+    name: "ASPA V2",
+    description:
+      "Full-stack developer in an agile team of 10 to develop the event registration web application for Auckland University Pool Association (ASPA) using MERN.",
+    image: "/projects/aspa.png",
+    github: "https://github.com/nroh555/aspa-portal-v2",
+  },
+  {
+    name: "Quick Draw",
+    description:
+      "A game to improve children's speed drawing skills. JavaFx and CSS was used to build the functionality and the UI while PyTorch was used to implement a ML model that predicts the users drawing.",
+    image: "/projects/quick-draw.png",
+    github: "https://github.com/nroh555/Quick-Draw-App",
+  },
+  {
+    name: "This Website",
+    description:
+      "The website that your are looking at right now. This website was created using NextJS, Typescript and Tailwind CSS.",
+    image: "/projects/portfolio.png",
+    github: "https://github.com/nroh555/Personal_Portfolio",
+  },
+];
+
+const Projects = () => {
+  return (
+    <section className="sm: pt-20" id="projects">
+      <div className="my-10 pb-10 lg:pt-10 lg:pb-10">
+        <h1 className="text-center font-bold text-4xl">
+          Projects
+          <hr className="w-10 h-1 mx-auto my-4 bg-blue-700 border-0 rounded"></hr>
+        </h1>
+        <div className="flex flex-col space-y-5">
+          {projects.map((project, index) => {
+            return (
+              <div key={index}>
+                <div className="flex flex-col md:flex-row md:space-x-12">
+                  <div className="mt-12 md:w-1/2">
+                    <Image
+                      src={project.image}
+                      alt=""
+                      width={1000}
+                      height={1000}
+                      className="rounded-xl shadow-xl"
+                    />
+                  </div>
+                  <div className="mt-12 md:w-1/2">
+                    <h1 className="text-2xl font-bold mb-2">{project.name}</h1>
+                    <p className="text-xl leading-7 mb-8 text-neutral-700 dark:text-neutral-100">
+                      {project.description}
+                    </p>
+                    <div>
+                      <Link href={project.github} target="_blank">
+                        <BsGithub
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
