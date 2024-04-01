8ee3a0921147063ee4b51bd6a59b71ebc08e537f
nroh555 <nroh555@aucklanduni.ac.nz>
2024-04-02 11:04:29 +1300

    Changed the experience section

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,21 +1,37 @@
 "use client";
 import React from "react";
 import Typed from "react-typed";
-import {VerticalTimeline,VerticalTimelineElement} from "react-vertical-timeline-component";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import Image from "next/image";
 
 const experiences = [
   {
     title: "Software Engineering Intern",
+    company_name: "EROAD",
+    icon: "/projects/code.png",
+    iconBg: "#E6DEDD",
+    date: "Nov 2023 - Feb 2024",
+    points: [
+      "Created a Jobs Support Tool that allowed tier 2,3 and product managers to investigate the full route lifecycle and troubleshoot issues for Sysco (our largest customer), decreasing investigation time by 100%",
+      "Worked on full-stack web development using React, Typescript and Tailwind CSS for the front-end as well as .NET and MongoDB for the REST backend following OpenAPI specification. This included unit and integration testing using React Testing Library and xUnit",
+      "Leveraged GitHub Actions for CI/CD deployment of containerised application using Docker into Azure Kubernetes Cluster",
+    ],
+  },
+  {
+    title: "Machine Learning Engineering Intern",
     company_name: "Spark Business Group",
     icon: "/projects/code.png",
     iconBg: "#E6DEDD",
     date: "Nov 2022 - Feb 2023",
-    points: ["Worked on Automated Road Inspection (ARI) for Fulton Hogan by assisting in the development of a machine-learning model that would determine cracks on roads using AWS Sagemaker, PyTorch and OpenCV.",
-             "Implemented semantic segmentation algorithm which predicted the mask model at 50% accuracy",
-             "Assisted in the development and migration of a Landing Zone Test codebase to AWS SDK Go V2."
-            ]
+    points: [
+      "Worked on Automated Road Inspection (ARI) for Fulton Hogan by assisting in the development of a machine-learning model that would determine cracks on roads using AWS Sagemaker, PyTorch and OpenCV.",
+      "Implemented semantic segmentation algorithm which predicted the mask model at 50% accuracy",
+      "Assisted in the development and migration of a Landing Zone Test codebase to AWS SDK Go V2.",
+    ],
   },
   {
     title: "Project Manager",
@@ -23,9 +39,11 @@ const experiences = [
     icon: "/projects/manager.png",
     iconBg: "#E6DEDD",
     date: "Mar 2023 - Oct 2023",
-    points: ["Leading an agile scrum team of 9 developers to build a web application to promote Taitamariki Potentia using Next.js and Firebase to 1000+ users.",
-            "The application has a content management system (CMS) using Notion that allows non-technical users to effortlessly change content on the website.",
-            "Working cross-functionally to define product roadmap,  requirements and deliverables with stakeholders, developers and designers."]
+    points: [
+      "Leading an agile scrum team of 9 developers to build a web application to promote Taitamariki Potentia using Next.js and Firebase to 1000+ users.",
+      "The application has a content management system (CMS) using Notion that allows non-technical users to effortlessly change content on the website.",
+      "Working cross-functionally to define product roadmap,  requirements and deliverables with stakeholders, developers and designers.",
+    ],
   },
 ];
 
@@ -38,8 +56,8 @@ const ExperienceCard = ({ experience }: any) => (
       <div className="flex justify-center items-center w-full h-full">
         <Image
           src={experience.icon}
-          height = {100}
-          width = {100}
+          height={100}
+          width={100}
           alt={experience.company_name}
           className="w-[60%] h-[60%] object-contain"
         />
@@ -47,21 +65,29 @@ const ExperienceCard = ({ experience }: any) => (
     }
   >
     <div>
-        <h3 className="text-white text-2xl font-bold">{experience.title}</h3>
-        {experience.date}
-        <p className = "text-2xl font-semibold" style = {{margin:0}}>{experience.company_name}</p>
+      <h3 className="text-white text-2xl font-bold">{experience.title}</h3>
+      {experience.date}
+      <p className="text-2xl font-semibold" style={{ margin: 0 }}>
+        {experience.company_name}
+      </p>
     </div>
     <ul className="mt-5 list-disc ml-5 space-y-2">
-        <li className="text-white-100 text-[14px] pl-1 tracking-wider">{experience.points[0]}</li>
-        <li className="text-white-100 text-[14px] pl-1 tracking-wider">{experience.points[1]}</li>
-        <li className="text-white-100 text-[14px] pl-1 tracking-wider">{experience.points[2]}</li>
+      <li className="text-white-100 text-[14px] pl-1 tracking-wider">
+        {experience.points[0]}
+      </li>
+      <li className="text-white-100 text-[14px] pl-1 tracking-wider">
+        {experience.points[1]}
+      </li>
+      <li className="text-white-100 text-[14px] pl-1 tracking-wider">
+        {experience.points[2]}
+      </li>
     </ul>
   </VerticalTimelineElement>
 );
 
 const Experience = () => {
   return (
-    <section className ="pt-20" id="experience">
+    <section className="pt-20" id="experience">
       <div className="my-10 pb-10 lg:pt-10 lg:pb-10">
         <Typed
           className="flex items-center justify-center font-bold text-4xl"
@@ -73,9 +99,7 @@ const Experience = () => {
         <hr className="w-10 h-1 mx-auto my-4 bg-[#006AFA] border-0 rounded"></hr>
       </div>
       <div className="flex flex-col">
-        <VerticalTimeline
-           lineColor ="#006AFA"
-           layout = {'2-columns'}>
+        <VerticalTimeline lineColor="#006AFA" layout={"2-columns"}>
           {experiences.map((experience, index) => (
             <ExperienceCard key={index} experience={experience} />
           ))}
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,39 +1,44 @@
 "use client";
-import React from 'react'
-import {AiOutlineGithub, AiOutlineLinkedin} from "react-icons/ai"
-import Image from 'next/image';
+import React from "react";
+import { AiOutlineGithub, AiOutlineLinkedin } from "react-icons/ai";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className= "mx-auto max-w-2xl px-4 sm:px-6 md:max-w-5xl">
-        <hr className= "w-full h-1 mx-auto mt-8 bg-[#006AFA] border-0"></hr>
-        <div className= "mx-auto p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
-            <div className='text-xl dark:text-white'>
-                <Image
-                  src={"/logo.png"}
-                  alt="logo"
-                  width={40}
-                  height={40}
-                  className="mb-3 mr-5 inline-block"
-                />
-                © 2023 Naren Rohan</div>
-            <div className="flex flex-row items-center justify-center space-x-2 mb-1">
-                <a href= "https://github.com/nroh555" rel = "noreferrer" target = "_blank">
-                    <AiOutlineGithub
-                      className= "hover:-translate-y-1 transition-transform cursor pointer dark:text-white"
-                      size = {40}
-                    />
-                </a>
-                <a href= "https://www.linkedin.com/in/naren-rohan/" rel = "noreferrer" target = "_blank">
-                    <AiOutlineLinkedin
-                      className= "hover:-translate-y-1 transition-transform cursor pointer dark:text-white"
-                      size = {40}
-                    />
-                </a>
-            </div>
+    <footer className="mx-auto max-w-2xl px-4 sm:px-6 md:max-w-5xl">
+      <hr className="w-full h-1 mx-auto mt-8 bg-[#006AFA] border-0"></hr>
+      <div className="mx-auto p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
+        <div className="text-xl dark:text-white">
+          <Image
+            src={"/logo.png"}
+            alt="logo"
+            width={40}
+            height={40}
+            className="mb-3 mr-5 inline-block"
+          />
+          © 2024 Naren Rohan
         </div>
+        <div className="flex flex-row items-center justify-center space-x-2 mb-1">
+          <a href="https://github.com/nroh555" rel="noreferrer" target="_blank">
+            <AiOutlineGithub
+              className="hover:-translate-y-1 transition-transform cursor pointer dark:text-white"
+              size={40}
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/naren-rohan/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineLinkedin
+              className="hover:-translate-y-1 transition-transform cursor pointer dark:text-white"
+              size={40}
+            />
+          </a>
+        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
